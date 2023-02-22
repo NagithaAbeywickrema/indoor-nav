0ae9ac3e81ca69e3cf568f897a7e7cbbbb80e96d
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2023-02-22 13:59:11 +0530

    Firestore integraion

--- Assets/Samples/ARCore Extensions/1.35.0/Persistent Cloud Anchor Sample/Scripts/FirestoreManager.cs
+++ Assets/Samples/ARCore Extensions/1.35.0/Persistent Cloud Anchor Sample/Scripts/FirestoreManager.cs
@@ -21,12 +21,80 @@ public class FirestoreManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public async void addAnchor(string name, string anchor_id, string anchor_type ) 
+    { 
+        AnchorData anchor = new AnchorData
+        {
+            Name = name;
+            AnchorID = anchor_id;
+            AnchorType = anchor_type;
+        };
+
+        try {
+            DocumentReference addedDocRef = await db.Collection("anchor").AddAsync(anchor);
+        } catch (Exception e){
+            Debug.Log(e);
+        } 
+
     }
-    
-}
+
+    public async void addPair(string anchor1_id, string anchor2_id, ) 
+    { 
+        Pair pair = new Pair
+        {
+            Anchor1ID = anchor1_id;
+            Anchor2ID = anchor2_id;
+        };
+
+        try {
+            DocumentReference addedDocRef = await db.Collection("pair").AddAsync(pair);
+        } catch (Exception e){
+            Debug.Log(e);
+        } 
+    }
+
+    public async void getAnchors() 
+    {
+        try
+        {
+            List<AnchorData> allAnchors = new List<AnchorData>();
+            this.db.Collection("anchor").GetSnapshotAsync().ContinueWithOnMainThread(task =>
+            {
+                foreach(var oneAnchor in task.Result)
+                {
+                    AnchorData anchor = oneShop.ConvertTo<AnchorData>();
+                    allAnchors.Add(anchor);
+                }
+                return allAnchors;
+            });
+
+        }
+        catch (Exception error)
+        {
+            Debug.LogError(error);
+        }
+    }
+
+    public async void getPairs() 
+    {
+        try
+        {
+            List<PairData> allPairs = new List<PairData>();
+            this.db.Collection("pair").GetSnapshotAsync().ContinueWithOnMainThread(task =>
+            {
+                foreach(var oneAnchor in task.Result)
+                {
+                    PairData pair = oneShop.ConvertTo<PairData>();
+                    allPairs.Add(pair);
+                }
+                return allPairs;
+            });
+
+        }
+        catch (Exception error)
+        {
+            Debug.LogError(error);
+        }
+    }
 
 }
